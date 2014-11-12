075b143f0694eeb72a98d4f1f8068e34484272b8
jw <jonwei@1-cloud.net>
2014-11-11 16:44:46 -0800

    Added a small script showing how to send RPC msg to cfgagent, using info in neutron.conf

---
+++ onecloud/rpc_tester.py
@@ -0,0 +1,35 @@
+import sys
+
+from neutron.openstack.common.rpc import proxy # ICEHOUSE_BACKPORT
+from neutron.plugins.cisco.common import cisco_constants as c_constants
+from oslo.config import cfg
+from neutron.common import config as common_config
+
+
+class TestRPCNotifier(proxy.RpcProxy):
+    BASE_RPC_API_VERSION = '1.0'
+
+    def __init__(self, topic=c_constants.CFG_AGENT_L3_ROUTING):
+        super(TestRPCNotifier, self).__init__(
+            topic=topic, default_version=self.BASE_RPC_API_VERSION)
+
+    def test_cast(self, method_name):
+        context = {}
+        self.cast(context,
+                  self.make_msg(method_name),
+                  topic='%s.%s' % (c_constants.CFG_AGENT_L3_ROUTING,
+                                   "localhost.localdomain"))
+
+
+def rpc_tester_main():
+
+    conf = cfg.CONF
+    common_config.parse(sys.argv[1:])
+    conf(project='neutron')
+
+    rpc_tester = TestRPCNotifier()
+    rpc_tester.test_cast("routers_updated")
+
+if __name__ == "__main__":
+    rpc_tester_main()
+
