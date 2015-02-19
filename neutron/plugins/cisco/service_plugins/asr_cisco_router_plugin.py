372f471862b1f3f0b00767493715631642f947a2
jw <jon.wei@onecloudinc.com>
2015-02-19 14:36:28 -0800

    Moved ASR l3 plugin to separate files

---
+++ neutron/plugins/cisco/service_plugins/asr_cisco_router_plugin.py
@@ -0,0 +1,94 @@
+from neutron.common import rpc as n_rpc
+from neutron.common import topics
+from neutron.db import agents_db
+# from neutron.db import common_db_mixin  # ICEHOUSE_BACKPORT
+from neutron.db import db_base_plugin_v2
+from neutron import manager
+from neutron.plugins.cisco.db.l3 import device_handling_db
+from neutron.plugins.cisco.db.l3 import asr_l3_router_appliance_db
+from neutron.plugins.cisco.l3.rpc import (l3_router_cfgagent_rpc_cb as
+                                          l3_router_rpc)
+from neutron.plugins.cisco.l3.rpc import devices_cfgagent_rpc_cb as devices_rpc
+from neutron.plugins.common import constants
+
+from neutron.openstack.common import rpc as o_rpc  # ICEHOUSE_BACKPORT
+from neutron.db import l3_rpc_base 
+
+
+DEVICE_OWNER_ROUTER_HA_INTF = "network:router_ha_interface"
+DEVICE_OWNER_ROUTER_HA_GW = "network:router_ha_gateway"
+PHYSICAL_GLOBAL_ROUTER_ID = "PHYSICAL_GLOBAL_ROUTER_ID"
+
+
+# class CiscoRouterPluginRpcCallbacks(n_rpc.RpcCallback, # ICEHOUSE_BACKPORT
+class CiscoRouterPluginRpcCallbacks(l3_router_rpc.L3RouterCfgRpcCallbackMixin,
+                                    l3_rpc_base.L3RpcCallbackMixin,
+                                    devices_rpc.DeviceCfgRpcCallbackMixin):
+    RPC_API_VERSION = '1.1'
+
+    def __init__(self, l3plugin):
+        super(CiscoRouterPluginRpcCallbacks, self).__init__()
+        self._l3plugin = l3plugin
+
+    @property
+    def _core_plugin(self):
+        return manager.NeutronManager.get_plugin()
+
+    def create_rpc_dispatcher(self):
+        return n_rpc.PluginRpcDispatcher([self])
+
+
+class PhysicalCiscoRouterPlugin(db_base_plugin_v2.CommonDbMixin,
+                                agents_db.AgentDbMixin,
+                                asr_l3_router_appliance_db.PhysicalL3RouterApplianceDBMixin,
+                                device_handling_db.DeviceHandlingMixin):
+
+    """Implementation of Cisco L3 Router Service Plugin for Neutron.
+
+    This class implements a L3 service plugin that provides
+    router and floatingip resources and manages associated
+    request/response.
+    All DB functionality is implemented in class
+    l3_router_appliance_db.L3RouterApplianceDBMixin.
+    """
+    supported_extension_aliases = ["router", "extraroute"]
+
+    def __init__(self):
+        self.setup_rpc()
+        # for backlogging of non-scheduled routers
+        self._setup_backlog_handling()
+        self._setup_device_handling()
+
+        self._phy_l3_mixin_init()
+
+    def setup_rpc(self):
+        # RPC support
+        self.topic = topics.L3PLUGIN
+        # self.conn = n_rpc.create_connection(new=True)  # ICEHOUSE_BACKPORT
+        self.conn = o_rpc.create_connection(new=True)
+        self.callbacks = CiscoRouterPluginRpcCallbacks(self)  # ICEHOUSE_BACKPORT
+        self.dispatcher = self.callbacks.create_rpc_dispatcher() # ICEHOUSE_BACKPORT
+        self.endpoints = [CiscoRouterPluginRpcCallbacks(self)]
+        # self.conn.create_consumer(self.topic, self.endpoints, # ICEHOUSE_BACKPORT
+        #                           fanout=False)
+        self.conn.create_consumer(self.topic, self.dispatcher,
+                                  fanout=False)
+        # Consume from all consumers in threads
+        # self.conn.consume_in_threads()  # ICEHOUSE_BACKPORT
+        self.conn.consume_in_thread()
+
+    def get_plugin_type(self):
+        return constants.L3_ROUTER_NAT
+
+    def get_plugin_description(self):
+        return ("Cisco Router Service Plugin for basic L3 forwarding"
+                " between (L2) Neutron networks and access to external"
+                " networks via a NAT gateway.")
+
+    @property
+    def _core_plugin(self):
+        try:
+            return self._plugin
+        except AttributeError:
+            self._plugin = manager.NeutronManager.get_plugin()
+            return self._plugin
