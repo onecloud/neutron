11257e6f13d6188dae6efb1cf67d606c48cd710f
Kahou Lei <kahou82@gmail.com>
2015-08-03 11:01:45 -0700

    User can only add one subnet per network in ASR1K model
    Currently ASR1K plugin doesn't support multiple subnets per network.
    We need to add validation check to disallow user to do that.
    
    This patchset introduces a metacloud option "subnet_per_network" to allow user
    to create a certain amount of subnet under a network.
    
    By default, the value is -1 which means we don't check the subnet count.
    For ASR1K model, we will set it as 1.
    
    Steps to reproduce
    1. Create one network (external or internal)
    2. Create one subnet under that network
    3. Create another subnet
    
    Fixes Jira: OPENSTACK-1717
    
    Change-Id: I975a9e3af7016f536e5b4ab45caa596ccc7b8b2c

--- neutron/db/db_base_plugin_v2.py
+++ neutron/db/db_base_plugin_v2.py
@@ -50,6 +50,14 @@ LOG = logging.getLogger(__name__)
 # IP allocations being cleaned up by cascade.
 AUTO_DELETE_PORT_OWNERS = [constants.DEVICE_OWNER_DHCP]
 
+metacloud_opts = [
+    cfg.IntOpt('subnet_per_network',
+               default=-1,
+               help='Number of subnet per network. Do not check the subnet'
+                    'count if it is a zero or a negative number'),
+]
+cfg.CONF.register_opts(metacloud_opts, group='metacloud')
+
 
 class NeutronDbPluginV2(neutron_plugin_base_v2.NeutronPluginBaseV2,
                         common_db_mixin.CommonDbMixin):
@@ -996,7 +1004,10 @@ class NeutronDbPluginV2(neutron_plugin_base_v2.NeutronPluginBaseV2,
             raise n_exc.InvalidInput(error_message=msg)
 
     def _validate_subnet(self, context, s, cur_subnet=None):
-        """Validate a subnet spec."""
+        """Validate a subnet spec.
+
+        MC Changes: Add _validate_subnet_count
+        """
 
         # This method will validate attributes which may change during
         # create_subnet() and update_subnet().
@@ -1067,6 +1078,25 @@ class NeutronDbPluginV2(neutron_plugin_base_v2.NeutronPluginBaseV2,
         if ip_ver == 6:
             self._validate_ipv6_attributes(s, cur_subnet)
 
+        self._validate_subnet_count(context, s)
+
+    def _validate_subnet_count(self, context, subnet):
+        subnet_per_network = cfg.CONF.metacloud.subnet_per_network
+        if subnet_per_network <= 0:
+            return True
+
+        net_id = subnet.get('network_id', None)
+        if not net_id:
+            raise n_exc.InvalidInput('Missing network id from subnet')
+
+        subnets = self._get_subnets_by_network(context, net_id)
+        if subnets and len(subnets) >= subnet_per_network:
+            err_msg = "Only %s subnet per network is allowed" % \
+                      subnet_per_network
+            raise n_exc.InvalidInput(error_message=(err_msg))
+
+        return True
+
     def _validate_gw_out_of_pools(self, gateway_ip, pools):
         for allocation_pool in pools:
             pool_range = netaddr.IPRange(
@@ -1078,7 +1108,6 @@ class NeutronDbPluginV2(neutron_plugin_base_v2.NeutronPluginBaseV2,
                     ip_address=gateway_ip)
 
     def create_subnet(self, context, subnet):
-
         net = netaddr.IPNetwork(subnet['subnet']['cidr'])
         # turn the CIDR into a proper subnet
         subnet['subnet']['cidr'] = '%s/%s' % (net.network, net.prefixlen)
---
+++ neutron/tests/unit/metacloud/test_db_base_plugin_v2.py
@@ -0,0 +1,61 @@
+# Copyright 2015 Cisco Systems, Inc.  All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from neutron.common import exceptions as n_exc
+from neutron.db import db_base_plugin_v2 as plugin
+from neutron.tests import base
+
+from oslo.config import cfg
+
+
+class TestNeutronDbPluginV2(base.BaseTestCase):
+    @mock.patch('neutron.db.db_base_plugin_v2.'
+                'NeutronDbPluginV2._get_subnets_by_network')
+    def test_subnet_count_validation(self, mock_get_subnet):
+        """Validate subnet count base on the metacloud options."""
+        db_api = plugin.NeutronDbPluginV2()
+
+        # Test no op if subnet_per_network is a default value (negative number)
+        self.assertTrue(db_api._validate_subnet_count(None, None))
+
+        # Test subnet count
+        cfg.CONF.set_override(name='subnet_per_network',
+                              override=0,
+                              group='metacloud')
+
+        # Test no op if subnet_per_network is a default value (zero)
+        self.assertTrue(db_api._validate_subnet_count(None, None))
+
+        # Test subnet count
+        cfg.CONF.set_override(name='subnet_per_network',
+                              override=1,
+                              group='metacloud')
+
+        # return 1 item
+        mock_get_subnet.return_value = [{}]
+
+        self.assertRaises(n_exc.InvalidInput,
+                          db_api._validate_subnet_count,
+                          None,
+                          {'network_id': 123})
+
+        cfg.CONF.set_override(name='subnet_per_network',
+                              override=2,
+                              group='metacloud')
+
+        mock_get_subnet.return_value = [{}]
+        self.assertTrue(db_api._validate_subnet_count(None,
+                                                      {'network_id': 123}))
